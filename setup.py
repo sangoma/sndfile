9ece07e38c796536694f2c4b70056697d6113169
Simon Gomizelj <simon@vodik.xyz>
2018-08-09 17:55:04 -0400

    Prepare v0.2.0 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 setup(
     name='sndfile',
-    version='0.1.2',
+    version='0.2.0',
     description='A CFFI wrapper around libsndfile',
     author='Simon Gomizelj',
     author_email='simon@vodik.xyz',
